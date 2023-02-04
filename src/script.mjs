55fcebe0010ff7c7a50cee6156ff26cbc22b716a
Donny C <99075588+Donny-C-1@users.noreply.github.com>
2023-02-04 21:49:09 +0100

    Completed the design of the side nav

--- src/script.mjs
+++ src/script.mjs
@@ -1,8 +1,19 @@
 window.addEventListener('DOMContentLoaded', setup);
 
 function setup() {
-    const element = document.createElement('h1');
-    element.textContent = "Hello World";
-    document.body.appendChild(element);
-    console.log('building');
+    navigation();
+}
+
+function navigation() {
+    const links = document.querySelectorAll("aside nav a");
+    const nav = document.querySelector("aside nav")
+    let prevActiveLink;
+    links.forEach(link => link.addEventListener('click', function(e) {
+        e.preventDefault();
+        prevActiveLink && prevActiveLink.classList.remove('active');
+        this.classList.add('active');
+        prevActiveLink = this;
+        nav.style.setProperty("--top", `${this.offsetTop}px`);
+        console.log(this.getAttribute('href'));
+    }));
 }
