276ed40595a22203dd09da192cc8a998e56e693a
Donny C <99075588+Donny-C-1@users.noreply.github.com>
2023-02-04 19:06:03 +0100

    Completed setup Build is now completely deployable with a script

--- gh-build.js
+++ gh-build.js
@@ -1,33 +1,10 @@
 const fs = require('fs');
 
-fs.readdir('./src', (err, files) => {
+const cb = (err) => {
     if (err) throw(err);
-
-    // *Retrieve all paths
-    const folders = files.filter(fileName => !(/.*\.(mj|j)s/.test(fileName)));
-    console.log(folders);
-
-    fs.copyFile('./src/images', './dist/images', err => {
-        if (err) throw(err);
-        console.log('File has been copied');
-    })
-
-
-    // *Remove all .js and .mjs files
-    // const nonCompileFiles = files.filter(file => /.*/.test(file));
-
-
-    console.log(files);
-})
-
-async function moveFilesInPath(path, cb) {
-    // todo Run some code to seperate all the folders, javascript files and normal files
-    // todo call this function with all the folders path
-    // todo Move all non javascript files
-
-    fs.readder(path, (err, files) => {
-        if (err) throw(err);
-
-        const folders = files.filter(fileName => /.*/.test(fileName));
-    })
+    console.log('File copied to dist');
 }
+
+fs.copyFile('./src/index.html', './dist/index.html', cb)
+
+fs.copyFile('./src/style.css', './dist/style.css', cb)
